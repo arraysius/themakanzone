18a2eaf545c142b553b306610b4fbc88d1541ff8
arraysius <aloy.cp@gmail.com>
2019-04-20 02:32:50 +0800

    Build commit

---
+++ precache-manifest.b2272a1434657133a20dcdfa99754e0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2c11c33f3a02b89b560",
+    "url": "/static/css/main.e641f78e.chunk.css"
+  },
+  {
+    "revision": "c2c11c33f3a02b89b560",
+    "url": "/static/js/main.d6e4207c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4d320fb7377c8bbae2e8",
+    "url": "/static/js/2.c47e719f.chunk.js"
+  },
+  {
+    "revision": "aed2e2d9c97e6ec263be82e5ac6555c4",
+    "url": "/index.html"
+  }
+];

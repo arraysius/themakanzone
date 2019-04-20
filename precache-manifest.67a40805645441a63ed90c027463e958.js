65ede76f45d84edc1404a6a9724392d6cacda497
arraysius <aloy.cp@gmail.com>
2019-04-20 21:57:08 +0800

    build

---
+++ precache-manifest.67a40805645441a63ed90c027463e958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2869ef47970d270ff212",
+    "url": "/themakanzone/static/css/main.e641f78e.chunk.css"
+  },
+  {
+    "revision": "2869ef47970d270ff212",
+    "url": "/themakanzone/static/js/main.a637a82a.chunk.js"
+  },
+  {
+    "revision": "477f3e05c7c5f0722119",
+    "url": "/themakanzone/static/js/runtime~main.0c9422dc.js"
+  },
+  {
+    "revision": "4d320fb7377c8bbae2e8",
+    "url": "/themakanzone/static/js/2.c47e719f.chunk.js"
+  },
+  {
+    "revision": "d267a59c49297a855f506c5e6fe0c8bf",
+    "url": "/themakanzone/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a3d087c3178ff182d7e7cc0110c694a0.js"
+  "/themakanzone/precache-manifest.67a40805645441a63ed90c027463e958.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/themakanzone/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
